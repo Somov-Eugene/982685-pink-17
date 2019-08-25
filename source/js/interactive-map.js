f5e33e654eeea8da35cb82d6041ad53a6a920164
Somov Eugene <somov.eugene.2012@gmail.com>
2019-08-25 22:18:59 +0300

    небольшие исправления в форматировании скрипта

--- source/js/interactive-map.js
+++ source/js/interactive-map.js
@@ -3,7 +3,7 @@ var mapElement = document.getElementById("map");
 if (mapElement) {
   var map = "";
 
-  var m_res = function () {
+  var map_resize = function () {
     google.maps.event.trigger(map, "resize");
     map.panTo(new google.maps.LatLng(59.936287, 30.321047));
   };
@@ -22,7 +22,8 @@ if (mapElement) {
     mapElement.classList.add("contacts-map--loaded");
 
     map = new google.maps.Map(mapElement, mapOptions);
-    var image = {
+
+    var mapMarker = {
       url: "../img/icon-map-marker.svg",
       size: new google.maps.Size(36, 36),
       scaledSize: new google.maps.Size(36, 36),
@@ -34,10 +35,10 @@ if (mapElement) {
       position: new google.maps.LatLng(59.936287, 30.321047),
       map: map,
       optimized: false,
-      icon: image
+      icon: mapMarker
     });
 
-    m_res();
+    map_resize();
   };
 
   google.maps.event.addDomListener(window, "load", init);
--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,20 +1,3 @@
-/*
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-*/
-
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 var pageHeader = document.querySelector(".page-header");
@@ -24,21 +7,26 @@ navMain.classList.remove("main-nav--nojs");
 pageHeader.classList.remove("page-header--full");
 pageIntro.classList.remove("intro--short");
 
-navToggle.addEventListener("click", function() {
+navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("main-nav--closed")) {
     navMain.classList.remove("main-nav--closed");
     navMain.classList.add("main-nav--opened");
+
     if (pageHeader.classList.contains("page-header--full")) {
       pageHeader.classList.remove("page-header--full");
-    } else {
+    }
+    else {
       pageHeader.classList.add("page-header--full");
-    };
+    }
+
     if (pageIntro.classList.contains("intro--short")) {
       pageIntro.classList.remove("intro--short");
-    } else {
+    }
+    else {
       pageIntro.classList.add("intro--short");
-    };
-  } else {
+    }
+  }
+  else {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
     pageHeader.classList.remove("page-header--full");
